50e309b3054835d682872fda3a814ceb2b115075
Ladzi <ladziladzani@gmail.com>
2023-06-27 12:43:16 +0200

    typeScript arrays

--- sandbox.js
+++ sandbox.js
@@ -16,3 +16,21 @@ var circumference = function (diameter) {
     return diameter * Math.PI;
 };
 console.log(circumference(5));
+// arrays and objects
+var arrayNames = ["john", "luigi", "yoshi"];
+// arrayNames.push(234) // error
+// arraynames[2] = 34; 
+arrayNames.push("toad");
+var numbers = [3, 34, 532, 523];
+numbers.push(23);
+numbers[3] = 342;
+//numbers.push("234");
+//numbers[1] = "234fxv";
+// when declaring the array at the start... and put different data-type values in it... typeScript will not produce any errors when you want to later add values to the same array...
+var mixedArray = ["james", 23, "farooq", 2342, 2334];
+mixedArray.push(24);
+;
+mixedArray[0] = 242;
+mixedArray.push("ronaldo");
+mixedArray[mixedArray.length] = 3;
+mixedArray.forEach(function (el) { return console.log(el); });
--- sandbox.ts
+++ sandbox.ts
@@ -23,3 +23,26 @@ console.log(fname);
     return diameter * Math.PI;
  }
  console.log(circumference(5));
+
+
+ // arrays and objects
+
+  let arrayNames = ["john", "luigi", "yoshi"];
+  // arrayNames.push(234) // error
+  // arraynames[2] = 34; 
+  arrayNames.push("toad");
+
+  let numbers = [3, 34, 532, 523];
+  numbers.push(23);
+  numbers[3] = 342;
+  //numbers.push("234");
+  //numbers[1] = "234fxv";
+
+
+// when declaring the array at the start... and put different data-type values in it... typeScript will not produce any errors when you want to later add values to the same array...
+  let mixedArray = ["james", 23, "farooq", 2342, 2334];
+  mixedArray.push(24);;
+  mixedArray[0] = 242;
+  mixedArray.push("ronaldo");
+  mixedArray[mixedArray.length] = 3;
+  mixedArray.forEach(el => console.log(el));
