7bd347902e19d25c82569b7662547b20c62e8471
Ladzi <ladziladzani@gmail.com>
2023-06-27 12:30:50 +0200

    types and functions

--- sandbox.js
+++ sandbox.js
@@ -4,3 +4,15 @@ var inputs = document.querySelectorAll('input');
 inputs.forEach(function (input) {
     console.log(input);
 });
+// typescript types
+var fname = "ladzi031";
+console.log(fname);
+//fname = 32; ... will result in an error
+var isBlackBelt = false;
+// isBlackBelt = "yes" ... results in an error
+isBlackBelt = true;
+var age = 18;
+var circumference = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circumference(5));
--- sandbox.ts
+++ sandbox.ts
@@ -5,3 +5,21 @@ const inputs = document.querySelectorAll('input');
 inputs.forEach((input)  => {
     console.log(input);
 });
+
+// typescript types
+
+let fname = "ladzi031";
+console.log(fname);
+//fname = 32; ... will result in an error
+
+ let isBlackBelt = false;
+ // isBlackBelt = "yes" ... results in an error
+ isBlackBelt = true;
+
+ let age = 18;
+
+
+ const circumference = (diameter: number) => {
+    return diameter * Math.PI;
+ }
+ console.log(circumference(5));
